5ca9f0490f863faf38c1e240dc0d3a121dc7f036
heqat <arturgouveia21@gmail.com>
2026-01-08 14:22:00 -0300

    feat: enhance hero section with gradient overlay and improved layout

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,8 @@ export default function Home() {
         <meta name="title" content="Carnaval 2026 - Bezerros-PE"></meta>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
+
+        
       </Head>
 
       <Navbar />
